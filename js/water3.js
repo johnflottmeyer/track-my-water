ac6f1872323ec92a8bf41afca24565a1def68287
john <john@voilamediagroup.com>
2016-05-20 14:57:35 -0500

    what the heck chart

--- js/water3.js
+++ js/water3.js
@@ -322,16 +322,17 @@ function renderGoal(tx,results){
        //end chart render
        //show what is saved
        $(".mygoaldata").html("<h3>"+insp+"</h3><table data-role='table' class='ui-responsive table-stroke table-alerts table-stripe' style='width:100%'><tr><thead><th>Date</th><th>Total</th></thead></tr><tr><tbody><td>" + date + "</td><td>" + s + "</td></tbody></table>"); 
-       if((goal-s)>0){
+       //lets set up the pie - donut chart
+       /*if((goal-s)>0){
 	       currentAmount = s;
 	       goaltotal = goal-x;
        }else{ //goal is reached
 	       goaltotal = 0;
 	       currentAmount = goal;
-       }
+       }*/
        donutTotal = Number(goal*2);
        new Chartist.Pie('.ct-chart', {
-		  series: [currentAmount,goaltotal]
+		  series: [s,goal]
 		}, {
 		  donut: true,
 		  donutWidth: 100,
