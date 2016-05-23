af2327abf60a00da9b1c421d1f77fd80e0562803
john <john@voilamediagroup.com>
2016-05-23 10:35:13 -0500

    goal needs to be saved / registered

--- js/water3.js
+++ js/water3.js
@@ -322,10 +322,11 @@ function renderGoal(tx,results){
        //end chart render
        //show what is saved
        $(".mygoaldata").html("<h3>"+insp+"</h3><table data-role='table' class='ui-responsive table-stroke table-alerts table-stripe' style='width:100%'><tr><thead><th>Date</th><th>Total</th></thead></tr><tr><tbody><td>" + date + "</td><td>" + s + "</td></tbody></table>"); 
+       
        //lets set up the pie - donut chart
-       if(!goal){
-	    	goal = 64;   
-       }
+       
+	    goal = 64;   //temporary 
+       
        if((goal-s)>0){
 	       currentAmount = s;
 	       goaltotal = goal-s;
@@ -347,7 +348,7 @@ function renderGoal(tx,results){
 		  plugins: [
             Chartist.plugins.fillDonut({
 	            items: [{
-                    content: '<h3>Goal <span class="small"> oz</span></h3>'
+                    content: '<h3>Goal <span class="small">'+ goal +' oz</span></h3>'
                 }]
 	        })
 	      ],
