9f961ad1eca45baf3db9e1fde0d9f5438bae3679
john <john@voilamediagroup.com>
2016-06-03 11:27:44 -0500

    testing updating the alerts settings

--- js/water3.js
+++ js/water3.js
@@ -859,14 +859,18 @@ $(document).ready(function() {
     /*ON PAGE LOAD SHOW THE FOLLOWING*/
     $(document).on("pagecontainerbeforeshow", function( event, ui ) {
     	var ThisPage = $(':mobile-pagecontainer').pagecontainer('getActivePage').attr('id');
-    	if(ThisPage == "alerts"){if(settings == "on"){switchedON();checkAlerts();}
+    	if(ThisPage == "alerts"){
+	    	if(settings == "on"){switchedON();checkAlerts();}
+	    	alert(settings);
     	}else if(ThisPage == "track"){getWater();
     	}else if(ThisPage == "account"){getGoal();}
     });
     
     $(document).on("pagecontainershow", function( event, ui ) {
     	var ThisPage = $(':mobile-pagecontainer').pagecontainer('getActivePage').attr('id');
-    	if(ThisPage == "alerts"){if(settings == "on"){switchedON();checkAlerts();}
+    	if(ThisPage == "alerts"){
+	    	if(settings == "on"){switchedON();checkAlerts();}
+	    	alert(settings);
     	}else if(ThisPage == "track"){getWater();
     	}else if(ThisPage == "account"){getGoal();
     	}
