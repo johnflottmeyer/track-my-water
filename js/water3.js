a9550d54fe98a231aa324049aeaeaabac8803f87
john <john@voilamediagroup.com>
2016-06-24 16:26:05 -0500

    testing healthkit data

--- js/water3.js
+++ js/water3.js
@@ -38,7 +38,8 @@ function checkNotificationPermissions(){ //LETS CHECK WHETHER IT HAS BEEN GRANTE
 }
 /*** HEALTHKIT ***/
 function onSuccess(result) { //can be removed when live
-	alert(result);//temp
+	var check = checkAuthStatus(HKQuantityTypeIdentifierDietaryWater);
+	alert(check);//temp
 	toastr.success('Healthkit Now Authorized', null, {target: $('.messages-home'),"timeOut": "3000","positionClass": "toast-top-full-width"}); 
 
 };
